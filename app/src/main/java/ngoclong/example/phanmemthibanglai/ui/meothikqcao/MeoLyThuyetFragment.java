f1a77c5bfd2fd31b2eb736b52d6706c95877c7bf
Tien181099 <65878150+Tien181099@users.noreply.github.com>
2020-08-03 11:10:25 +0700

    capnhat
    aa

--- app/src/main/java/ngoclong/example/phanmemthibanglai/ui/meothikqcao/MeoLyThuyetFragment.java
+++ app/src/main/java/ngoclong/example/phanmemthibanglai/ui/meothikqcao/MeoLyThuyetFragment.java
@@ -8,8 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -20,13 +24,23 @@ import ngoclong.example.phanmemthibanglai.R;
  */
 public class MeoLyThuyetFragment extends Fragment {
 
-    Spinner spinner,spinner2,spinner3;
+
     ListView listView;
-    String[] items = {"1","2","3","4","5","6","7","8","9"};
+    private MeoThiDAO mt;
+    private ArrayList<MeoThi> arrMeoThi;
+    TextView tvNoiDung;
+    int count;
     public MeoLyThuyetFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mt = new MeoThiDAO(getContext());
+        arrMeoThi = new ArrayList<MeoThi>();
+        count = mt.getMeoLyThuyet();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,5 +50,40 @@ public class MeoLyThuyetFragment extends Fragment {
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listView = view.findViewById(R.id.listLT);
+        loadAllMLT();
     }
+    private void loadAllMLT() {
+        arrMeoThi = mt.getAllMeoLyThuyet();
+        CustomAdapter customAdapter = new CustomAdapter();
+        listView.setAdapter(customAdapter);
+    }
+    private class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return arrMeoThi.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+            View view1 = getLayoutInflater().inflate(R.layout.item_meothi, null);
+            tvNoiDung = view1.findViewById(R.id.tvMLT);
+
+            tvNoiDung.setText(arrMeoThi.get(i).getNoiDung());
+            return view1;
+        }
+    }
+
 }
